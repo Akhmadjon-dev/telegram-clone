516f9aced995f1f4b5e64d1a8764ea756b5b9019
Ahmadjon <info.ahmadjon@gmail.com>
2020-10-26 18:40:32 +0500

    almost ready message container

--- src/components/Thread/Thread.js
+++ src/components/Thread/Thread.js
@@ -1,17 +1,21 @@
 import React from 'react'
-import {Avatar} from '@material-ui/core';
+import {Avatar, IconButton} from '@material-ui/core';
+import {MoreHoriz} from '@material-ui/icons';
 import './Thread.css'
 const Thread = () => {
     return (
         <div className="thread">
             <div className="thread__header">
-                <div className="thead__header__contents">
+                <div className="thread__header__contents">
                     <Avatar />
                     <div className="thread__header__contents__info">
                         <h4>ThreadName</h4>
                         <h5>Last Seen</h5>
                     </div>
                 </div>
+            <IconButton>
+                <MoreHoriz className="thread__header__details"/>
+            </IconButton>
             </div>
         </div>
     )
